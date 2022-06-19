ad5bfa1c9d1edfd040af697316fd38dd60138927
zhange2 <38747622+zhange2@users.noreply.github.com>
2022-06-19 21:56:36 +0100

    Update concerttickets.cpp

--- concerttickets.cpp
+++ concerttickets.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
-
+#define ll long long
+#define vt vector
 using namespace std;
-
-// variables used for the current problem
 int n, m, h, t;
 multiset<int> tickets;
 
@@ -35,5 +34,4 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     solve();
-    return 0;
 }
