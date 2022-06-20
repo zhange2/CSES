049a62e60ee8adedea98dc4d3bd0f36766930654
Edward Zhang <38747622+zhange2@users.noreply.github.com>
2022-06-20 20:21:56 +0100

    Update dicecombinations.cpp

--- dicecombinations.cpp
+++ dicecombinations.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int modulo = 1e9 + 7;
+#define ll long long
+#define vt vector
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define dbg(c) cout << #c << '=' << c << endl
+#define sz(x) (int)(x).size()
 int main()
 {
-    long long n, ans;
+    ll n, ans;
     cin>>n;
-    long long arr[n+1] = {0};
+    ll arr[n+1] = {0};
     for (int i=1;i<=n;i++) {
         arr[0] = 1;
         for (int x=1;x<=6;x++) {
