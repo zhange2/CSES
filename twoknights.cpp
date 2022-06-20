95059c47bbd6dcc1e93e90319282dcdc344ed778
Edward Zhang <38747622+zhange2@users.noreply.github.com>
2022-06-20 20:21:29 +0100

    Update twoknights.cpp

--- twoknights.cpp
+++ twoknights.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define vt vector
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define dbg(c) cout << #c << '=' << c << endl
+#define sz(x) (int)(x).size()
+
 ll solve(ll n) {
     return (n * n) *(n * n - 1) / 2 - 4 * (n-1) * (n-2);
 }
